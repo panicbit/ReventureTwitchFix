b17d7750a6ca9ff3a2f1804fb6a1dc044bff1f49
panicbit <panicbit.dev@gmail.com>
2025-10-29 03:02:33 +0100

    fix emote parsing crash

--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,11 @@
-﻿using BepInEx;
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
+using TwitchLib.Client;
+using TwitchLib.Client.Models;
+using TwitchLib.Client.Models.Internal;
 
 namespace ReventureTwitchFix;
 
@@ -7,11 +13,35 @@ namespace ReventureTwitchFix;
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
-        
+
     private void Awake()
     {
         // Plugin startup logic
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        Harmony.CreateAndPatchAll(typeof(Plugin));
+    }
+
+    [HarmonyPatch(typeof(TwitchClient), "HandleIrcMessage")]
+    [HarmonyFinalizer]
+    static void HandleIrcMessage(Exception __exception)
+    {
+        if (__exception != null)
+        {
+            Logger.LogError(">>> Exception caught: " + __exception.ToString());
+        }
+    }
+
+    [HarmonyPatch(typeof(TwitchClient), "HandlePrivMsg")]
+    [HarmonyPrefix]
+    static void HandlePrivMsg(IrcMessage ircMessage)
+    {
+        if (ircMessage.Hostmask.Equals("jtv!jtv@jtv.tmi.twitch.tv"))
+        {
+            return;
+        }
+
+        ircMessage.Tags.Remove("emotes");
     }
 }
